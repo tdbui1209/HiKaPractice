288c31f825ab3e14f68c87783027dde342629095
tdbui1209 <tdbui1209@gmail.com>
2023-07-21 01:15:45 +0700

    add question remainung, total question, log result

--- FormKatakana.Designer.cs
+++ FormKatakana.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox_answer = new System.Windows.Forms.TextBox();
             this.button_submit = new System.Windows.Forms.Button();
             this.label_result = new System.Windows.Forms.Label();
+            this.label_question_remaining = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_question)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,6 +41,7 @@
             this.pictureBox_question.Location = new System.Drawing.Point(269, 26);
             this.pictureBox_question.Name = "pictureBox_question";
             this.pictureBox_question.Size = new System.Drawing.Size(244, 239);
+            this.pictureBox_question.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_question.TabIndex = 0;
             this.pictureBox_question.TabStop = false;
             // 
@@ -50,10 +52,11 @@
             this.textBox_answer.Name = "textBox_answer";
             this.textBox_answer.Size = new System.Drawing.Size(159, 30);
             this.textBox_answer.TabIndex = 1;
+            this.textBox_answer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_answer_KeyDown);
             // 
             // button_submit
             // 
-            this.button_submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_submit.Location = new System.Drawing.Point(434, 386);
             this.button_submit.Name = "button_submit";
             this.button_submit.Size = new System.Drawing.Size(79, 29);
@@ -64,24 +67,38 @@
             // 
             // label_result
             // 
-            this.label_result.AutoSize = true;
+            this.label_result.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.label_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_result.Location = new System.Drawing.Point(311, 283);
+            this.label_result.Location = new System.Drawing.Point(0, 53);
             this.label_result.Name = "label_result";
-            this.label_result.Size = new System.Drawing.Size(161, 32);
+            this.label_result.Size = new System.Drawing.Size(782, 500);
             this.label_result.TabIndex = 3;
             this.label_result.Text = "label_result";
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label_result.Visible = false;
+            // 
+            // label_question_remaining
+            // 
+            this.label_question_remaining.AutoSize = true;
+            this.label_question_remaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_question_remaining.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_question_remaining.Location = new System.Drawing.Point(12, 26);
+            this.label_question_remaining.Name = "label_question_remaining";
+            this.label_question_remaining.Size = new System.Drawing.Size(234, 25);
+            this.label_question_remaining.TabIndex = 4;
+            this.label_question_remaining.Text = "label_question_remaining";
             // 
             // Katakana
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
             this.ClientSize = new System.Drawing.Size(782, 553);
-            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label_question_remaining);
             this.Controls.Add(this.button_submit);
             this.Controls.Add(this.textBox_answer);
             this.Controls.Add(this.pictureBox_question);
+            this.Controls.Add(this.label_result);
             this.Name = "Katakana";
             this.Text = "FormKatakana";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_question)).EndInit();
@@ -96,5 +113,6 @@
         private System.Windows.Forms.TextBox textBox_answer;
         private System.Windows.Forms.Button button_submit;
         private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Label label_question_remaining;
     }
 }
--- FormKatakana.cs
+++ FormKatakana.cs
@@ -15,42 +15,82 @@ namespace HiKaPractice
     {
         string image_path;
         string correct_answer;
+        int total_question;
+        int question_remaining;
         int score;
+
+        List<string> history_answer = new List<string>();
+        List<string> history_correct_answer = new List<string>();
+
         KatakanaQuiz katakanaQuiz;
         public Katakana()
         {
             InitializeComponent();
             katakanaQuiz = new KatakanaQuiz();
             score = 0;
+            total_question = katakanaQuiz.get_total_question();
+            question_remaining = total_question;
             draw();
         }
 
         void draw()
         {
-            string[] draw = katakanaQuiz.Draw();
-            image_path = draw[0];
-            correct_answer = draw[1];
-            pictureBox_question.Image = Image.FromFile(image_path);
+            label_question_remaining.Text = "Số câu còn lại: " + question_remaining.ToString();
+            try
+            {
+                string[] draw = katakanaQuiz.Draw();
+                image_path = draw[0];
+                correct_answer = draw[1];
+                pictureBox_question.Image = Image.FromFile(image_path);
+            }
+            catch (Exception ex)
+            {
+                Form form = new FormResult(total_question, score, history_answer, history_correct_answer);
+                form.ShowDialog();
+            }
         }
 
-        void button_submit_Click(object sender, EventArgs e)
+        void submit_answer()
         {
             String answer = textBox_answer.Text;
             answer = answer.Trim().ToLower();
+
             if (answer.Length > 0 && answer == correct_answer)
             {
                 score++;
                 label_result.Text = "正しい";
-                label_result.Visible = true;
                 label_result.ForeColor = Color.Green;
             }
             else
             {
-                label_result.Text = "間違い";
-                label_result.Visible = true;
+                label_result.Text = "間違い!" + " Đáp án đúng là: " + correct_answer;
                 label_result.ForeColor = Color.Red;
             }
+            label_result.Visible = true;
+            label_result.TextAlign = ContentAlignment.MiddleCenter;
             draw();
+            textBox_answer.Text = "";
+            question_remaining--;
+        }
+
+        void button_submit_Click(object sender, EventArgs e)
+        {
+            submit_answer();
+        }
+
+        private void button_submit_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox_answer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                submit_answer();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
         }
     }
 }
